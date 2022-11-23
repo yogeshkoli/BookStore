53c4ce460c2d23d9297017e43f6c3024b66efb9e
yogesh.koli <yogeshkoli@hotmail.com>
2022-11-23 18:55:48 +0530

    Adding Custom Errro Message to ModelState at ServerSide

--- BookStore/Controllers/CategoryController.cs
+++ BookStore/Controllers/CategoryController.cs
@@ -36,6 +36,10 @@ public class CategoryController : Controller
     [ValidateAntiForgeryToken]
     public IActionResult Create(Category category)
     {
+        if(category.Name == category.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("Name", "The DisplayOrder cannot exactly match the Name.");
+        }
         if (ModelState.IsValid)
         {
             _store_context.Categories.Add(category);
