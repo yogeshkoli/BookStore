31fa422172abb38e5781e0615d3a6fca66e015c4
yogesh.koli <yogeshkoli@hotmail.com>
2022-11-24 09:47:49 +0530

    Applying a Ranage Validation on Input Field with Custom Error Message

--- BookStore/Models/Category.cs
+++ BookStore/Models/Category.cs
@@ -15,8 +15,9 @@ namespace BookStore.Models
 
         [Required]
         public string Name { get; set; }
-        
+
         [DisplayNameAttribute("Display Order")]
+        [Range(0, 100, ErrorMessage ="Display Order must be between 1 and 100 only!")]
         public int DisplayOrder { get; set; }
 
         [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Computed)]
