1eb7cdb1e2db079b628f51a7f5461e8bfe407650
yogesh.koli <yogeshkoli@hotmail.com>
2022-12-02 10:08:02 +0530

    Removed unecessory usings statements

--- BookStore/Data/StoreContext.cs
+++ BookStore/Data/StoreContext.cs
@@ -1,9 +1,4 @@
 namespace BookStore.Data;
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using BookStore.Models;
 
--- BookStore/Models/Category.cs
+++ BookStore/Models/Category.cs
@@ -1,9 +1,5 @@
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 
 namespace BookStore.Models
