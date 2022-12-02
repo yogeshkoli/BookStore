93ed307c2eec139c181504c58ecf4213cbaf86c0
yogesh.koli <yogeshkoli@hotmail.com>
2022-12-02 10:09:00 +0530

    removed usings

--- BookStore/Controllers/CategoryController.cs
+++ BookStore/Controllers/CategoryController.cs
@@ -1,10 +1,6 @@
 namespace BookStore.Controllers;
 
-using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using BookStore.Models;
