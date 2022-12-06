ba0b25bdaaf9a5dbe40764e4d4595653b4836327
yogesh.koli <yogeshkoli@hotmail.com>
2022-12-06 20:45:51 +0530

    Created a Product Controller

---
+++ BookStore/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,115 @@
+namespace BookStore.Areas.Admin.Controllers;
+
+using System.Collections.Generic;
+using BookStore.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BookStore.Models;
+
+public class ProductController : Controller
+{
+    private readonly ILogger<ProductController> _logger;
+
+    private readonly IUnitOfWork _iUnitOfWork;
+
+    public ProductController(ILogger<ProductController> logger, IUnitOfWork iUnitOfWork)
+    {
+        _logger = logger;
+        _iUnitOfWork = iUnitOfWork;
+    }
+
+    public IActionResult Index()
+    {
+        IEnumerable<Product> productList = _iUnitOfWork.iProductRepository.GetAll();
+        return View(productList);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Product product)
+    {
+        if (ModelState.IsValid)
+        {
+            _iUnitOfWork.iProductRepository.Add(product);
+            _iUnitOfWork.Save();
+            TempData["success"] = "New Product has been successfully created";
+            return RedirectToAction("Index");
+        }
+
+        return View();
+    }
+
+    public IActionResult Edit(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        var product = _iUnitOfWork.iProductRepository.GetFirstOrDefault(p => p.Id == id);
+
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        return View(product);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Product product)
+    {
+        if (ModelState.IsValid)
+        {
+            _iUnitOfWork.iProductRepository.Update(product);
+            _iUnitOfWork.Save();
+            TempData["success"] = "Product has been successfully updated.";
+            return RedirectToAction("Index");
+        }
+
+        return View();
+    }
+
+    public IActionResult Delete(int? id)
+    {
+        var product = _iUnitOfWork.iProductRepository.GetFirstOrDefault(p => p.Id == id);
+
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        return View(product);
+    }
+
+    [HttpPost]
+    [ActionName('Delete')]
+    public IActionResult handleDelete(int? id)
+    {
+        var product = _iUnitOfWork.iProductRepository.GetFirstOrDefault(p => p.Id == id);
+
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        _iUnitOfWork.iProductRepository.Remove(product);
+        _iUnitOfWork.Save();
+        TempData["success"] = "Given product deleted successfully.";
+
+        return RedirectToAction("Index");
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View("Error!");
+    }
+}
