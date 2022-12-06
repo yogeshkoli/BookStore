109abfb0aeb264f673214315ba11e2daa8df1d54
yogesh.koli <yogeshkoli@hotmail.com>
2022-12-06 11:31:45 +0530

    Added Product model & a DbSet

--- BookStore.DataAccess/StoreContext.cs
+++ BookStore.DataAccess/StoreContext.cs
@@ -12,4 +12,6 @@ public class StoreContext : DbContext
     public DbSet<Category> Categories { get; set; }
 
     public DbSet<CoverType> CoverTypes { get; set; }
+
+    public DbSet<Product> Products { get; set; }
 }
---
+++ BookStore.Models/Product.cs
@@ -0,0 +1,60 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string ISBN { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        [Range(1, 10000)]
+        public double ListPrice { get; set; }
+
+        [Required]
+        [Range(1, 10000)]
+        public double Price { get; set; }
+
+        [Required]
+        [Range(1, 10000)]
+        public double Price50 { get; set; }
+
+        [Required]
+        [Range(1, 10000)]
+        public double Price100 { get; set; }
+
+        public string Image { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [ForeignKeyAttribute("CategoryId")]
+        public Category Category { get; set; }
+
+        [Required]
+        public int CoverTypeId { get; set; }
+
+        [ForeignKeyAttribute("CoverTypeId")]
+        public CoverType CoverType { get; set; }
+
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Computed)]
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
